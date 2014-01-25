630434b741cf2d486c7c20fd3f6a06972d663ae6
Jason Teplitz <jteplitz602@gmail.com>
2014-01-25 01:56:22 -0800

    Added fix for camera rotation so that the kinext doesn't flip a shit
    Also removed temp directory from repo.

--- Unity/Assets/Scripts/GameController.js
+++ Unity/Assets/Scripts/GameController.js
@@ -2,21 +2,31 @@
 class Tower{
   var TowerObject  : GameObject;
   var camera : Camera;
+
+  private var cameraRotationOffset : Quaternion;
+
+  function setCameraRotationOffset(offset : Quaternion){
+    cameraRotationOffset = offset;
+  }
+  function getCameraRotationOffset(){
+    return cameraRotationOffset;
+  }
 };
 
 // globals
 var towers: Tower[] = [
 ];
+var currentRotation : float = 0.0; // keep track of how far we've rotated
 
 private var currentTower : Tower;
 
 // functions
+var setupCameraRotations : Function;
 var switchToTower : Function;
 var switchToCamera : Function; var checkInput : Function; var rotateView : Function;
 
 function Start () {
-  // put the cameras in the camera object
-  // is there a better way to do this and keep the variables unity nice?
+  setupCameraRotations();
   switchToTower(towers[0]);
 }
 
@@ -45,22 +55,36 @@ rotateView = function(left : boolean){
   if (left){
     degrees *= -1;
   }
-  currentTower.camera.transform.RotateAround(currentTower.TowerObject.transform.position, Vector3.up, degrees * Time.deltaTime);
+  var rotateAmount = degrees * Time.deltaTime;
+  currentRotation = (currentRotation + rotateAmount) % 360;
+  currentTower.camera.transform.RotateAround(currentTower.TowerObject.transform.position, Vector3.up, rotateAmount);
 };
 
 switchToTower = function (tower : Tower){
   // switch to this tower's camera
-  switchToCamera(tower.camera);
+  switchToCamera(tower.camera, tower.getCameraRotationOffset(), tower.TowerObject);
   currentTower = tower;
 };
 
-switchToCamera = function (camera : Camera){
+switchToCamera = function (camera : Camera, initOffset : Quaternion, towerObject : GameObject){
   camera.enabled = true;
+  // set this camera's rotation such that it won't fuck up the kinect
+  camera.transform.rotation = initOffset;
+  camera.transform.RotateAround(towerObject.transform.position, Vector3.up, currentRotation);
+  // disable the other camera's
   for (var i = 0; i < towers.length; i++){
     var currCamera : Camera = towers[i].camera;
     if (currCamera !== camera){
       currCamera.GetComponent(AudioListener).enabled = false;
       currCamera.enabled = false;
     }
+  }
+};
+
+// saves the initial y rotation of each of the tower's cameras for use later
+setupCameraRotations = function(){
+  for (var i : int = 0; i < towers.length; i++){
+    var currTower : Tower = towers[i];
+    currTower.setCameraRotationOffset(currTower.camera.transform.rotation);
   }
 };
